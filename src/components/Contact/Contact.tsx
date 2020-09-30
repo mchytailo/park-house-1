5f87304656f9c9b847f23f2faf4b2a09d11477d0
mchytailo <foa18FghK!>
2020-09-30 12:24:39 +0300

    Update

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,64 +1,62 @@
 import React, {FC, useEffect, useRef, useState} from 'react';
 import {ContactHeader, ContactLine, ContactSection, ContactText} from "./Contact.styled";
-import contactEmbed from './contactEmbed'
+import contactEmbed from './contactEmbed';
 
 interface IProps {
-    windowWidth: number
+	windowWidth: number
 }
 
 const Contact: FC<IProps> = ({windowWidth}) => {
-    const [form, setForm] = useState<HTMLElement | null>(null)
-    const [counter, setCounter] = useState<number>(0)
-    const contactFormRef = useRef<HTMLDivElement>(null);
-    useEffect(() => {
-        const script = document.createElement("script");
-        script.innerHTML = contactEmbed;
-        script.type = 'text/javascript';
-        script.id = 'aoform-script-65a7048d-2c63-41a9-86d3-c56af2931016:d-0004';
-        contactFormRef && contactFormRef.current && contactFormRef.current.appendChild(script);
-    }, [contactFormRef])
+	const [form, setForm] = useState<HTMLElement | null>(null);
+	const [counter, setCounter] = useState<number>(0);
+	const contactFormRef = useRef<HTMLDivElement>(null);
+	useEffect(() => {
+		const script = document.createElement("script");
+		script.innerHTML = contactEmbed;
+		script.type = 'text/javascript';
+		script.id = 'aoform-script-65a7048d-2c63-41a9-86d3-c56af2931016:d-0004';
+		contactFormRef && contactFormRef.current && contactFormRef.current.appendChild(script);
+	}, [contactFormRef]);
 
-    const logSubmit = () => {
-        const checkErrors = document.getElementsByClassName('ao-form-error').length;
-        if (!checkErrors && contactFormRef && contactFormRef.current) {
-            if(windowWidth < 992){
-                window.scrollTo({
-                    top: contactFormRef.current.offsetTop || 0,
-                })
-            }
-            let contactText = contactFormRef.current.getElementsByClassName('contact-text')[0] as HTMLElement;
-            let contactLine = contactFormRef.current.getElementsByClassName('contact-line')[0] as HTMLElement;
-            let contactHeader = contactFormRef.current.getElementsByClassName('contact-header')[0] as HTMLElement;
-            contactFormRef.current.removeChild(contactText);
-            contactFormRef.current.removeChild(contactLine);
-            contactFormRef.current.removeChild(contactHeader);
-        }
-    }
+	const logSubmit = () => {
+		const checkErrors = document.getElementsByClassName('ao-form-error').length;
+		if (!checkErrors && contactFormRef && contactFormRef.current) {
+			window.scrollTo({
+				top: contactFormRef.current.offsetTop || 0,
+			});
+			let contactText = contactFormRef.current.getElementsByClassName('contact-text')[0] as HTMLElement;
+			let contactLine = contactFormRef.current.getElementsByClassName('contact-line')[0] as HTMLElement;
+			let contactHeader = contactFormRef.current.getElementsByClassName('contact-header')[0] as HTMLElement;
+			contactFormRef.current.removeChild(contactText);
+			contactFormRef.current.removeChild(contactLine);
+			contactFormRef.current.removeChild(contactHeader);
+		}
+	};
 
-    useEffect(() => {
-        if (!form && counter < 20) {
-            const interval = setInterval(() => {
-                setCounter(count => count + 1)
-                const getForm = document.getElementById('ao-form-65a7048d-2c63-41a9-86d3-c56af2931016');
-                setForm(getForm);
-                if (getForm)
-                    getForm.addEventListener('submit', logSubmit)
-            }, 1000);
-            return () => clearInterval(interval);
-        } else return;
-    }, [form, counter]);
+	useEffect(() => {
+		if (!form && counter < 20) {
+			const interval = setInterval(() => {
+				setCounter(count => count + 1);
+				const getForm = document.getElementById('ao-form-65a7048d-2c63-41a9-86d3-c56af2931016');
+				setForm(getForm);
+				if (getForm)
+					getForm.addEventListener('submit', logSubmit);
+			}, 1000);
+			return () => clearInterval(interval);
+		} else return;
+	}, [form, counter]);
 
-    return (
-        <ContactSection ref={contactFormRef}>
-            <ContactText className={'contact-text'}>
-                To stay updated about Parkhouse please register below and we will keep you informed. Thank you.
-            </ContactText>
-            <ContactLine className={'contact-line'}/>
-            <ContactHeader className={'contact-header'}>
-                REGISTER
-            </ContactHeader>
-        </ContactSection>
-    )
-}
+	return (
+		<ContactSection ref={contactFormRef}>
+			<ContactText className={'contact-text'}>
+				To stay updated about Parkhouse please register below and we will keep you informed. Thank you.
+			</ContactText>
+			<ContactLine className={'contact-line'}/>
+			<ContactHeader className={'contact-header'}>
+				REGISTER
+			</ContactHeader>
+		</ContactSection>
+	);
+};
 
 export default Contact;
